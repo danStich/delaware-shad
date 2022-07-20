0292ce9cbe8d1be2676d43ebf9b8ea968f10e6b6
danStich <daniel.stich@oneonta.edu>
2022-07-20 13:18:47 -0400

    recode group mods

--- rps_yn_stan.R
+++ rps_yn_stan.R
@@ -6,7 +6,7 @@ library(tidyr)
 library(reshape2)
 
 # . Set options ----
-options(mc.cores = parallel::detectCores(4))  
+options(mc.cores = parallel::detectCores())  
 rstan_options(auto_write = TRUE)
 
 # . Data Read ----
@@ -14,16 +14,17 @@ fish <- read.csv("data/SHADMASTER.csv", stringsAsFactors = FALSE)
 
 # . Data manipulation ----
 fish <- fish %>% 
-  filter(!is.na(final_age) & !is.na(fork) & !is.na(live_dead) & !is.na(cohort))
+  filter(!is.na(final_age) & !is.na(fork) & !is.na(rps) & !is.na(cohort))
 
 # Run model ----
+# Package the data for stan
 # Package the data for stan
 vb_data = list(
   length = fish$fork,
   age = fish$final_age,
   nobs = nrow(fish),
-  group = as.numeric(as.factor(fish$live_dead)),
-  ngroups = length(unique(fish$live_dead)),
+  group = as.numeric(as.factor(fish$rps)),
+  ngroups = length(unique(fish$rps)),
   hp_tau = 1,
   hp_sigma = 10,
   hp_omega = 4,
@@ -32,52 +33,46 @@ vb_data = list(
 )
 
 
+
 # Fit the model with stan
-live_dead_fit <- stan(file = 'models/vonbert_group.stan',
-                 data = vb_data,
-                 chains = 3,
-                 iter = 200,
-                 warmup = 100,
-                 control = list(
-                   adapt_delta = 0.80,
-                   max_treedepth = 10
-                 )
+rps_fit <- stan(file = 'models/vonbert_group.stan',
+                      data = vb_data,
+                      chains = 3,
+                      iter = 200,
+                      warmup = 100,
+                      control = list(
+                        adapt_delta = 0.80,
+                        max_treedepth = 10
+                      )
 )
 
 # Print model summary
-print(live_dead_fit, digits=3)
+print(rps_fit, digits=3)
 
 # Save result to a file
-save(live_dead_fit, file='results/vonbert_live_dead.rda')
+save(rps_fit, file='results/vonbert_rps.rda')
 
 # Results ----
 # . Load result ----
 # load("vonbert_covariate.rda")
 
 # . Get parameter estimates ----
-pars <- rstan::extract(live_dead_fit)  
-
-# - Results ----
-# . Load result ----
-# load("vonbert_live_dead.rda")
-
-# . Get parameter estimates ----
-pars <- rstan::extract(live_dead_fit)  
+pars <- rstan::extract(rps_fit)  
 
 linf_mat <- reshape2::melt(pars$Linf)  
-names(linf_mat) <- c("iteration", "live_dead", "linf")  
+names(linf_mat) <- c("iteration", "rps", "linf")  
 
 k_mat <- reshape2::melt(pars$K)  
-names(k_mat) <- c("iteration", "live_dead", "k")  
+names(k_mat) <- c("iteration", "rps", "k")  
 
 t0_mat <- reshape2::melt(pars$t0)  
-names(t0_mat) <- c("iteration", "live_dead", "t0")  
+names(t0_mat) <- c("iteration", "rps", "t0")  
 
 ests <- data.frame(linf_mat, k_mat$k, t0_mat$t0)
 names(ests)[3:5] <- c("linf", "k", "t0")
 
 ests <- ests %>% 
-  filter(live_dead != 3)
+  filter(rps != "unk")
 
 # . Summarize parameter estimates ----
 par_ests <- tidyr::pivot_longer(
@@ -88,7 +83,7 @@ par_ests <- tidyr::pivot_longer(
 )
 
 par_summary <- par_ests %>% 
-  group_by(live_dead, parameter) %>% 
+  group_by(rps, parameter) %>% 
   summarize(fit = mean(estimate),
             lwr = quantile(estimate, 0.025),
             upr = quantile(estimate, 0.975)
@@ -97,16 +92,16 @@ par_summary <- par_ests %>%
 
 
 # Make some violin plot
-ggplot(par_ests, aes(x=factor(live_dead), y=estimate), 
-       color=factor(sex), fill=factor(live_dead))+
+ggplot(par_ests, aes(x=factor(rps), y=estimate), 
+       color=factor(sex), fill=factor(rps))+
   geom_violin(alpha=.10)+
   facet_wrap(~parameter, scales = "free")
 
 #Make a scatter plot
-ggplot(par_summary,aes(x=factor(live_dead), y=fit), 
+ggplot(par_summary,aes(x=factor(rps), y=fit), 
        color=factor(sex), fill=factor(sex))+
   geom_point()+
-  geom_segment(aes(xend=factor(live_dead), y=lwr, yend=upr))+
+  geom_segment(aes(xend=factor(rps), y=lwr, yend=upr))+
   facet_wrap(~parameter, scales = "free")
 
 
@@ -121,7 +116,7 @@ vb_pred <- function(age, linf, k, t0) {
 out_list <- vector(mode = "list", length = length(Age))
 for(i in 1:length(unique(Age))){
   out_list[[i]] <- ests %>% 
-    group_by(iteration, live_dead) %>% 
+    group_by(iteration, rps) %>% 
     summarise(pred = vb_pred(Age[i], linf, k, t0))
   out_list[[i]]$Age = Age[i]
 }
@@ -131,20 +126,21 @@ preds <- do.call(rbind, out_list)
 #Summarize preds
 
 fish_preds <- preds %>% 
-  group_by(live_dead, Age) %>% 
+  group_by(rps, Age) %>% 
   summarise(fit=mean(pred),
             lwr=quantile(pred, 0.025),
             upr=quantile(pred, 0.975))
 
-fish$live_dead <- as.numeric(as.factor(fish$live_dead))
+
+fish$rps <- as.numeric(as.factor(fish$rps))
 
 fish <- fish %>% 
-  filter(live_dead != 3)
+  filter(rps != 3)
 
 # Plot predictions
 ggplot(fish_preds, aes(x = Age, y = fit)) +
   geom_line(aes(y = fit)) +
-  facet_wrap(~live_dead) +
+  facet_wrap(~rps) +
   geom_ribbon(aes(xmax=Age, ymin=lwr, ymax=upr), alpha =.1) +
   geom_point(data = fish, aes(x=final_age, y = fork), alpha = 0.05) +
   theme_bw() +
